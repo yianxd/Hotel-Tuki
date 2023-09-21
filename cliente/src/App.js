2e6f828d00dad57bdf3b2be25e2dd9ac945d5e86
yianxd <jean4520rodriguez@gmail.com>
2023-09-21 17:16:54 -0500

    out of context 1

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -4,6 +4,9 @@ import {useState} from "react";
 
 import Axios from "axios";
 
+import 'boostrap/dist/css/boostrap.min.css';
+import Swal from "sweetaler2";
+
 function App() {
   return (
     <div className="App">
