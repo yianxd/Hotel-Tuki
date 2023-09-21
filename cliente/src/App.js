3efa40b27fef7f1c5a633aecef6bdbc89065f8d5
yianxd <jean4520rodriguez@gmail.com>
2023-09-21 18:36:02 -0500

    Axios, bootstrap y sweetaler2

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+
 import {useState} from "react";
+
 import Axios from "axios";
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+import 'boostrap/dist/css/boostrap.min.css';
 import Swal from "sweetaler2";
-//  
+
 function App() {
   return (
-    <div className="container">
-      <div className='card text-center'>
-      <div className='card-header'>
-        GESTION DE USUARIOS
-      </div>
-      <div className='card-body'>
-        
-
-      </div>
-
-
-      </div>
-
+    <div className="App">
+      
     </div>
   );
 }
