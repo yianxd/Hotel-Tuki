c44d01921f50495de67d035485dd997c71669683
yianxd <jean4520rodriguez@gmail.com>
2023-09-24 12:57:27 -0500

    arreglando errores...creo

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -9,6 +9,41 @@ import Swal from "sweetaler2";
 
 //<div></div>
 function App() {
+  const [id_usuario,setId_usuario] = useState(0);
+  const [usuario, setUsuario] = useState("");
+  const [nombre,setNombre] = useState("");
+  const [apellido,setApellido] = useState("");
+  const [correo,setCorreo] = useState("");
+  const [password,setPassword] = useState("");
+  const [id_tipo, setId_tipo] = useState("");
+
+    const add =()=>{
+      Axios.post("http://localhost:3001/create",{
+        id_usuario: id_usuario,
+        usuario: usuario,
+        nombre: nombre,
+        apellido: apellido,
+        correo: correo,
+        password: password,
+        id_tipo: id_tipo
+      }).then(()=>{
+        listar();
+        clear();
+        Swal.fire({
+          title: "<strong>Registro Existoso!</strong>",
+          html:"<i> Ek usuario <strong>"+nombre+"</strong> fue registrado con exito!!</i>",
+          icon: 'success',
+          timer:3000
+        })
+      }).catch(function(error){
+        FileSystemWritableFileStream.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: JSON.parse(JSON.stringify(error)).message==="Network Error"?"intente mas tarde":JSON.parse(JSON.stringify(error)).message
+        })
+      });
+    }
+
   return (
     <div className="container">
       <div className='card tex-center'>
@@ -55,13 +90,25 @@ function App() {
             className='form-control' value={password}/>
           </div>
           <div className='info'>
-            <label>Nombre de usuario</label>
-            <input type='text' onChange={event => {setUsuario(event.target.value);
+            <label>Tipo de usuario</label>
+            <input type='text' onChange={event => {setId_tipo(event.target.value="3");
             }}
-            className='form-control' value={usuario}/>
+            className='form-control' value={id_tipo}/>
           </div>
         </div>
-      </div>  
+
+        <div className='card-footer text-muted'>
+          {
+            editar?
+            <div>
+              <button className='btn btn-warning m-2' onClick={update}>Actualizar</button>
+              <button className='btn btn-info m-2' onClick={clear}>Cancelar</button>
+            </div>
+            :<button className='btn btn-succes' onClick={add}>Registrar</button>
+          }
+              <button className='btn btn-secondary' onClick={listar}>Listar</button>
+        </div>
+      </div>
       </div>    
     </div>
   );
---
+++ servidor/index.js
@@ -0,0 +1,37 @@
+const express = requiere("express");
+const app = express();
+const mysql = requiere("mysql");
+const cors = requiere("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"",
+    database:"db_hotel"
+})
+
+app.post("/create",(req,res)=>{
+    const id_usuario = req.body.id_usuario;
+    const usuario = req.body.usuario;
+    const nombre = req.body.nombre;
+    const apellido = req.body.apellido;
+    const correo = req.body.correo;
+    const password = req.body.correo;
+    const id_tipo = req.body.id_tipo;
+
+    db.query('INSERT INTO datos_usuario(id_usuario,usuario,nombre,apellido,correo,password,id_tipo) VALUE(?,?,?,?,?,?,?)',[id_usuario,usuario,nombre,apellido,correo,password,id_tipo],(error,result)=>{
+        if(error){
+            console.log(error);
+        }else{
+            res.send("registro exitoso")
+        }
+    })
+})
+
+app.listen(3001,()=>{
+    console.log("puerto activado")
+})
+
